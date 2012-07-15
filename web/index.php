5f0477bcf82716501e1d739bf4ae11b370ec6c3f
Martin Indra <aktive@seznam.cz>
2012-07-15 17:45:39 +0200

    Writing content of project web page.

--- web/index.php
+++ web/index.php
@@ -2,15 +2,58 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>CRPP - Collaborative raster painting protocol</title>
-        <style>
-
-        </style>
+        <link rel="stylesheet" href="css/main.css" type="text/css" />
     </head>
     <body>
+        <div id="menu">
+            Menu
+        </div>
+        <div id="content">
+            <h1>CRPP - Collaborative raster painting protocol</h1>
 
-        <h1>CRPP - Collaborative raster painting protocol</h1>
+            <p>CRPP je síťový protokol pro sdílené (rastrové) kreslení mezi mnoha uživately v reálném čase.</p>
+            
+            <h2>Licence</h2>
 
-        <p>Web page is under construction.</p>
+            <p>Protokol je šířen pod svobodnou licencí, kdokoli ho může svobodně využívat. Viz další odstavec.</p>
+
+            <p>Protokol a jeho dokumentace je šířen pod licencí GNU FDL verze 1.3 nebo novější. Aktuální znění GNU FDL licence najdete na webové adrese <a href="http://www.gnu.org/copyleft/fdl.html">www.gnu.org/copyleft/fdl.html</a>. Veškeré texty tohoto webu jsou šířeny pod toutéž licencí (GNU FLD verze 1.3 nebo novější). Zdrojový kód tohoto webu je šířen pod licencí GNU GPL verze 3, jejíž znění najdete na webové adrese <a href="http://www.gnu.org/copyleft/gpl.html">www.gnu.org/copyleft/gpl.html</a>. Všechny zdrojové soubory jsou volně ke stažení z našeho veřejného GIT repozitáře. Viz sekce versioning (GIT) dále na této stránce.</p>
+
+            <h2>Náš cíl</h2>
+
+            <ul>
+                <li>Navrhnout a zdokumentovat CRPP protokol.</li>
+                <li>Protokol včetně dokumentace šířit pod svobodnou licencí.</li>
+                <li>Protokol by měl splňovat vlastnosti:
+                    <ul>
+                        <li>jednoduchý a minimalistický</li>
+                        <li>dostatečně obecný</li>
+                        <li>stabilní</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <h2>List of know implementation</h2>
+
+            <ul>
+                <li><a href="http://collab.mgn.cz">Collab</a> - miltiplatform client, server and frameworks based on this protocol.</li>
+            </ul>
+
+            <h2>CRPP protocol documentation</h2>
+
+            <p>Documentation is under development. Its current version you can download on our GitHub repository <a href="https://github.com/MoonGames/crpp/raw/master/documentation/protocol.pdf">github.com/.../protocol.pdf</a>.</p>
+
+            <h2>For developers</h2>
+
+            <h3>Versioning (GIT)</h3>
+
+            <p>All project git repository is on GitHub (<a href="https://github.com/MoonGames/crpp">github.com/MoonGames/crpp</a>). Use "git clone git://github.com/MoonGames/crpp.git" command to get current version.</p>
+
+            <h3>Wiki</h3>
+
+            <p>Developers wiki of this project is <a href="http://crpp-wiki.mgn.cz/">crpp-wiki.mgn.cz</a>.</p>
+
+        </div>
 
     </body>
 </html>
