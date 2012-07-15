4bc6e3d7b7906ebe035cb599a2a453aeb9850687
Martin Indra <aktive@seznam.cz>
2012-07-15 16:59:50 +0200

    ...

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>CRPP - Collaborative raster painting protocol</title>
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+        <h1>CRPP - Collaborative raster painting protocol</h1>
+
+        <p>Web page is under construction.</p>
+
+    </body>
+</html>
