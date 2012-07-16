9600d1ed144a87f91c818205008ed60d057b3206
Martin Indra <aktive@seznam.cz>
2012-07-16 20:37:28 +0200

    WEB: adding menu, make several sections, info about mailing list.

--- web/index.php
+++ web/index.php
@@ -6,53 +6,33 @@
     </head>
     <body>
         <div id="menu">
-            Menu
+
+            <h2>Menu</h2>
+
+            <?php
+            $items = array(
+                "about" => "about",
+                "mailing_list" => "mailing list"
+            );
+            ?>
+
+            <ul>
+                <?php
+                foreach ($items as $key => $value) {
+                    echo '<li>' . '<a href="?page=' . $key . '">' . $value . '</a>' . '</li>' . "\n";
+                }
+                ?>
+            </ul>
         </div>
         <div id="content">
-            <h1>CRPP - Collaborative raster painting protocol</h1>
-
-            <p>CRPP is network protocol for shared raster drawing among many people (users) in real-time. It means that several people can draw to same canvas in same time.</p>
-            
-            <h2>Licence</h2>
-
-            <p>Protocol is distributed under free/liberal licence. Anybody can use it for free. See next paragraph.</p>
-
-            <p>Protocol and his documentation is distributed under licence GNU FDL version 1.3 or greater. Current version of GNU FDL can be found on web address <a href="http://www.gnu.org/copyleft/fdl.html">www.gnu.org/copyleft/fdl.html</a>. All texts of this web are distributed under GNU FDL version 1.3 or greater too. Source code of this web is distributed under licence GNU GPL version 3. Text of licence GNU GPL version 3 is on site <a href="http://www.gnu.org/copyleft/gpl.html">www.gnu.org/copyleft/gpl.html</a>. All source files are free to download from our public GIT repository. See section versioning (GIT) on this page.</p>
-
-            <h2>Our goal</h2>
-
-            <ul>
-                <li>Design and document CRPP protocol.</li>
-                <li>Use liberal/free licence for protocol.</li>
-                <li>Features of protocol:
-                    <ul>
-                        <li>simple and minimalistic</li>
-                        <li>general</li>
-                        <li>stable</li>
-                    </ul>
-                </li>
-            </ul>
-
-            <h2>List of known implementations</h2>
-
-            <ul>
-                <li><a href="http://collab.mgn.cz">Collab</a> - miltiplatform client, server and frameworks based on this protocol.</li>
-            </ul>
-
-            <h2>CRPP protocol documentation</h2>
-
-            <p>Documentation is under development. Its current version you can download on our GitHub repository <a href="https://github.com/MoonGames/crpp/raw/master/documentation/protocol.pdf">github.com/.../protocol.pdf</a>.</p>
-
-            <h2>For developers</h2>
-
-            <h3>Versioning (GIT)</h3>
-
-            <p>All project git repository is on GitHub (<a href="https://github.com/MoonGames/crpp">github.com/MoonGames/crpp</a>). Use "git clone git://github.com/MoonGames/crpp.git" command to get current version.</p>
-
-            <h3>Wiki</h3>
-
-            <p>Developers wiki of this project is <a href="http://crpp-wiki.mgn.cz/">crpp-wiki.mgn.cz</a>.</p>
-
+            <?php
+            $page = $_GET["page"];
+            if (empty($page)) {
+                $page = "about";
+            }
+            $page = 'pages/' . $page . '.php';
+            require $page;
+            ?>
         </div>
 
     </body>
